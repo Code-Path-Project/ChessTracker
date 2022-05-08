40af3325ea19ed20256147e4c7aa46bb2475d438
abijitj <abijit.jay@gmail.com>
2022-05-07 17:22:52 -0700

    Add Android project

---
+++ app/src/main/java/com/example/chesstracker/FriendListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chesstracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FriendListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_list)
+    }
+}
