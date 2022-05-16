1e2db5044c10f1b07bf81cb5997010c8eaa4fa2d
yih365 <68409635+gigi365@users.noreply.github.com>
2022-05-16 10:57:59 -0700

    added navigation bar

--- app/src/main/java/com/example/chesstracker/MainActivity.kt
+++ app/src/main/java/com/example/chesstracker/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.chesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // navigation bar
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+            item ->
+            when(item.itemId) {
+                R.id.action_main -> {
+                    Toast.makeText(this, "main page", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.action_friends -> {
+                    Toast.makeText(this, "friend page", Toast.LENGTH_SHORT).show()
+
+                }
+
+                R.id.action_settings -> {
+                    Toast.makeText(this, "settings page", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+            true
+        }
+
     }
 }
