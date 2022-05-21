1eb158d73dc17359613f2273beb86613327bcc29
ChiSengWong <95706415+ChiSengWong@users.noreply.github.com>
2022-05-20 17:27:10 -0700

    mainAc converted to fragment

--- app/src/main/java/com/example/chesstracker/Fragments/MainFragment.kt
+++ app/src/main/java/com/example/chesstracker/Fragments/MainFragment.kt
@@ -1,34 +1,27 @@
 package com.example.chesstracker.Fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.chesstracker.R
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.example.chesstracker.*
+import okhttp3.Headers
+import org.json.JSONException
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+private const val PLAYER_GAME_HISTORY_URL = "https://api.chess.com/pub/player/erik/games/2022/05"
+
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val gameHistory = mutableListOf<Game>()
+    private lateinit var rvGameHistory: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +31,43 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val gameHistoryAdapter = GameHistoryAdapter(requireContext(), gameHistory)
+        rvGameHistory = view.findViewById(R.id.rv_gameHistory)
+        rvGameHistory.adapter = gameHistoryAdapter
+        rvGameHistory.layoutManager = LinearLayoutManager(requireContext())
+
+        rvGameHistory.addItemDecoration( // Adding divider line between view
+            DividerItemDecoration(
+                rvGameHistory.getContext(),
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+        val client = AsyncHttpClient()
+
+        client.get(PLAYER_GAME_HISTORY_URL, object : JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG, "onSuccess Json: $json")
+                try {
+                    val gameHistoryJsonArray = json.jsonObject.getJSONArray("games")
+                    gameHistory.addAll(Game.fromJsonArray(gameHistoryJsonArray))
+                    gameHistoryAdapter.notifyDataSetChanged()
+                    Log.i(TAG, "gameHistory list: $gameHistory")
+                }catch (e: JSONException){
+                    Log.e(TAG, "Encountered exception $e")
                 }
             }
+
+        })
     }
+
 }
--- app/src/main/java/com/example/chesstracker/MainActivity.kt
+++ app/src/main/java/com/example/chesstracker/MainActivity.kt
@@ -12,52 +12,14 @@ import okhttp3.Headers
 import org.json.JSONException
 
 
-private const val PLAYER_GAME_HISTORY_URL = "https://api.chess.com/pub/player/erik/games/2022/05"
 const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
-
-    private val gameHistory = mutableListOf<Game>()
-    private lateinit var rvGameHistory: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val gameHistoryAdapter = GameHistoryAdapter(this, gameHistory)
-        rvGameHistory = findViewById(R.id.rv_gameHistory)
-        rvGameHistory.adapter = gameHistoryAdapter
-        rvGameHistory.layoutManager = LinearLayoutManager(this)
 
-        rvGameHistory.addItemDecoration( // Adding divider line between view
-            DividerItemDecoration(
-                rvGameHistory.getContext(),
-                DividerItemDecoration.VERTICAL
-            )
-        )
-
-
-        val client = AsyncHttpClient()
-
-        client.get(PLAYER_GAME_HISTORY_URL, object : JsonHttpResponseHandler() {
-            override fun onFailure(
-                statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
-            ) {
-                Log.e(TAG, "onFailure $statusCode")
-            }
-
-            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
-                Log.i(TAG, "onSuccess Json: $json")
-                try {
-                    val gameHistoryJsonArray = json.jsonObject.getJSONArray("games")
-                    gameHistory.addAll(Game.fromJsonArray(gameHistoryJsonArray))
-                    gameHistoryAdapter.notifyDataSetChanged()
-                    Log.i(TAG, "gameHistory list: $gameHistory")
-                }catch (e: JSONException){
-                    Log.e(TAG, "Encountered exception $e")
-                }
-            }
-
-        })
     }
 }
