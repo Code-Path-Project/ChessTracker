a3e445c556b121aaa414e7f772abfe4327e96729
ChiSengWong <95706415+ChiSengWong@users.noreply.github.com>
2022-05-20 17:53:14 -0700

    error attribute chart_bg_color not found

--- app/src/main/java/com/example/chesstracker/MainActivity.kt
+++ app/src/main/java/com/example/chesstracker/MainActivity.kt
@@ -3,23 +3,55 @@ package com.example.chesstracker
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.example.chesstracker.Fragments.FriendsFragment
+import com.example.chesstracker.Fragments.MainFragment
+import com.example.chesstracker.Fragments.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import okhttp3.Headers
 import org.json.JSONException
 
 
 const val TAG = "MainActivity"
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // navigation bar
+        val fragmentManager: FragmentManager = supportFragmentManager
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+                item ->
 
+            var fragmentToShow: Fragment? = null
+            when(item.itemId) {
+                R.id.action_main -> {
+                    fragmentToShow = MainFragment()
+                }
+
+                R.id.action_friends -> {
+                    fragmentToShow = FriendsFragment()
+                }
+
+                R.id.action_settings -> {
+                    fragmentToShow = SettingsFragment()
+                }
+            }
+
+            if (fragmentToShow != null) {
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+            }
+
+            true
+        }
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_main
 
     }
 }
