d83739252de0df490e362190a294b2f57efc6b7d
abijitj <abijit.jay@gmail.com>
2022-05-21 17:26:10 -0700

    Add logout button in SettingsFragment

--- app/src/main/java/com/example/chesstracker/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/chesstracker/Fragments/SettingsFragment.kt
@@ -1,13 +1,17 @@
 package com.example.chesstracker.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CompoundButton
 import android.widget.Switch
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.chesstracker.LoginActivity
+import com.example.chesstracker.MainActivity
 import com.example.chesstracker.R
 
 class SettingsFragment : Fragment() {
@@ -30,9 +34,12 @@ class SettingsFragment : Fragment() {
             //Toast.makeText(this@MainActivity, message,
               //  Toast.LENGTH_SHORT).show()
         })
+
+        view.findViewById<Button>(R.id.btn_logout).setOnClickListener(){
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-
 
     /*override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         //setPreferencesFromResource(R.xml.root_preferences, rootKey)
